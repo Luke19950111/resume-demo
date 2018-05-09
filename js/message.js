44e7387c8d835f4e84dc65e79ac895ef5b1d57fe
Luke <lukelovemoney@gmail.com>
2018-05-09 16:14:30 +0800

    MVC

--- js/message.js
+++ js/message.js
@@ -1,54 +1,66 @@
-var APP_ID = 'YAfN7zEh3RzC9E2AaV6RWifr-gzGzoHsz';
-var APP_KEY = 'yRIGfz1brddLppELmYtR3zrD';
+!function () {
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
+    var view = document.querySelector('section.message')
 
-var query = new AV.Query('Message');
-query.find()
-    .then(function (messages) {
-        let array = messages.map((item) => item.attributes)
-        array.forEach((item) => {
-            let li = document.createElement('li')
-            li.innerText = `${item.name}: ${item.content}`
-            let messageList = document.querySelector('#messageList')
-            messageList.appendChild(li)
-        })
-    })
+    var controller = {
+        view: null,
+        messageList: null,
+        init: function (view) {
+            this.view = view
+            this.messageList = view.querySelector('#messageList')
+            this.myForm = view.querySelector('#postMessageForm')
+            this.initAV()
+            this.loadMessages()
+            this.bindEvents()
+        },
+        initAV: function () {
+            var APP_ID = 'YAfN7zEh3RzC9E2AaV6RWifr-gzGzoHsz';
+            var APP_KEY = 'yRIGfz1brddLppELmYtR3zrD';
+            AV.init({ appId: APP_ID, appKey: APP_KEY });
+        },
+        loadMessages: function () {
+            var query = new AV.Query('Message');
+            query.find()
+                .then((messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}: ${item.content}`
+                        this.messageList.appendChild(li)
+                    })
+                })
+        },
+        bindEvents: function () {           
+            this.myForm.addEventListener('submit', function (e) {
+                e.preventDefault()
+            this.saveMessage()
+            })
+        },
+        saveMessage: function () {
+            let myForm = this.myForm
+            let name = myForm.querySelector('input[name=name]').value
+            let content = myForm.querySelector('input[name=content]').value //content就是用户输入的信息
+
+            var Message = AV.Object.extend('Message');
+            var message = new Message();
+            message.save({
+                name: name,
+                content: content
+            }).then(function (object) {
+                let li = document.createElement('li')
+                li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li) //提交成功，直接在页面添加一个li，不再刷新页面
+                myForm.querySelector('input[name=content]').value = ''
+            })
+        }
+    }
 
 
 
-let myForm = document.querySelector('#postMessageForm')
-myForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    let name = myForm.querySelector('input[name=name]').value 
-    let content = myForm.querySelector('input[name=content]').value //content就是用户输入的信息
 
-    var Message = AV.Object.extend('Message');
-    var message = new Message();
-    message.save({
-        name: name,
-        content: content
-    }).then(function (object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.appendChild(li) //提交成功，直接在页面添加一个li，不再刷新页面
-        myForm.querySelector('input[name=content]').value = ''
-    })
-})
-/*
-//创建TestObject表
-var TestObject = AV.Object.extend('TestObject');
-//在表中创建一行数据
-var testObject = new TestObject();
-//数据内容是 words: 'Hello World!' 保存
-//如果保存成功，运行 alert''
-testObject.save({
-  words: 'Save World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
-*/
+
+
+
+    controller.init(view)
+}.call()
